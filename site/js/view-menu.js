1d5f7a28f68c7fa092ea957ee0044edb5319a769
DimaNovik <dimanovik1989@gmail.com>
2017-04-14 13:54:24 +0300

    Started create my new site-portfolio. Created left-menu

---
+++ site/js/view-menu.js
@@ -0,0 +1,20 @@
+
+var link = document.getElementById('open-menu');
+
+link.onclick = function() {
+	var divMenu = document.getElementById("left-menu");
+	var allMenu = document.getElementById("content-menu");
+
+	if(divMenu.style.width === '') {
+		link.innerHTML = '';
+		link.innerHTML = "MENU";
+		divMenu.style.width = '20%';
+		divMenu.style.transition='1s';
+		allMenu.style.visibility = 'visible';
+	} else {
+		divMenu.style.width = '';
+		link.innerHTML = "<>";
+		allMenu.style.visibility = 'hidden';
+	}
+	
+}
