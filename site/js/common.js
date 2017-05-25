d5340827e42c5465a8794cecccbdbe4865b45cb1
DimaNovik <dimanovik1989@gmail.com>
2017-05-25 12:04:46 +0300

    Optimize js code for changePages

--- site/js/common.js
+++ site/js/common.js
@@ -1,36 +1,19 @@
 function ChangePages(pages) {
-	let page = pages,
-		welcome = document.getElementById('welcome-page').id,
-		skills = document.getElementById('skills-page').id,
-		project = document.getElementById('project-page').id,
-		other = document.getElementById('other-page').id;
+	let getPage = pages,
+		main = document.getElementById('main-content').children,
+		ololo;
 
-	if(page === welcome) {
-		document.getElementById(page).style.display = "flex";
-		document.getElementById("skills-page").style = '';
-		document.getElementById("project-page").style = '';
-		document.getElementById("other-page").style = '';			
-	}
+	for (let i=0; i<main.length; i++) {
 
-	if(page === skills) {
-		document.getElementById(page).style.display = "block";
-		document.getElementById("welcome-page").style.display = 'none';
-		document.getElementById("project-page").style.display = 'none';
-		document.getElementById("other-page").style.display = 'none';			
-	}
+		pageName = main[i].id;
 
-	if(page === project) {
-		document.getElementById(page).style.display = "block";
-		document.getElementById("welcome-page").style.display = 'none';
-		document.getElementById("skills-page").style.display = 'none';
-		document.getElementById("other-page").style.display = 'none';		
-	}
+		if (getPage === pageName) {
+			main[i].style.display = "flex";
+		} else {
+			main[i].style.display = "none";
+		}
 
-	if(page === other) {
-		document.getElementById(page).style.display = "block";
-		document.getElementById("welcome-page").style.display = 'none';
-		document.getElementById("skills-page").style.display = 'none';
-		document.getElementById("project-page").style.display = 'none';		
+		
 	}
 
 }
