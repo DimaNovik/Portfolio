5d7ab8febb929c57066583fae1b919b8ad7e8ba2
DimaNovik <dimanovik1989@gmail.com>
2017-05-25 11:05:25 +0300

    Added to site refferer links.

--- site/js/common.js
+++ site/js/common.js
@@ -1,11 +1,36 @@
 function ChangePages(pages) {
 	let page = pages,
-		welcome = document.getElementById('welcome-page'),
-		skills = document.getElementById('skills-page'),
-		project = document.getElementById('project-page');
+		welcome = document.getElementById('welcome-page').id,
+		skills = document.getElementById('skills-page').id,
+		project = document.getElementById('project-page').id,
+		other = document.getElementById('other-page').id;
 
-	if(page === 'skills-page') {
-		document.getElementById("welcome-page").style.display = "none";
+	if(page === welcome) {
+		document.getElementById(page).style.display = "flex";
+		document.getElementById("skills-page").style = '';
+		document.getElementById("project-page").style = '';
+		document.getElementById("other-page").style = '';			
+	}
+
+	if(page === skills) {
+		document.getElementById(page).style.display = "block";
+		document.getElementById("welcome-page").style.display = 'none';
+		document.getElementById("project-page").style.display = 'none';
+		document.getElementById("other-page").style.display = 'none';			
+	}
+
+	if(page === project) {
+		document.getElementById(page).style.display = "block";
+		document.getElementById("welcome-page").style.display = 'none';
+		document.getElementById("skills-page").style.display = 'none';
+		document.getElementById("other-page").style.display = 'none';		
+	}
+
+	if(page === other) {
+		document.getElementById(page).style.display = "block";
+		document.getElementById("welcome-page").style.display = 'none';
+		document.getElementById("skills-page").style.display = 'none';
+		document.getElementById("project-page").style.display = 'none';		
 	}
 
 }
